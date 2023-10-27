eaa659cde43f83dda540caa94140cd34a1770d67
Andrew Morrow <andrew.morrow@viam.com>
2023-10-27 14:58:16 -0400

    RSDK-5103 `cargo generate --path ../../micro-rdk-module-template`

---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+use micro_rdk::common::registry::{ComponentRegistry, RegistryError};
+
+pub fn register_models(_registry: &mut ComponentRegistry) -> anyhow::Result<(), RegistryError> {
+    Ok(())
+}
